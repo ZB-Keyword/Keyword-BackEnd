6093abbe628f5d2693ca8c8917f1e44fff949d8a
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2024-01-02 20:39:38 +0900

    Update V1__create_table.sql

--- src/main/resources/db/migration/V1__create_table.sql
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -5,13 +5,13 @@ CREATE TABLE `Member` (
                           `phone`	VARCHAR(15)	NOT NULL,
                           `email`	VARCHAR(50)	NOT NULL,
                           `password`	VARCHAR(100)	NOT NULL,
-                          `status`	ENUM	NOT NULL,
+                          `status`	CHAR(15) NOT NULL,
                           `created_at`	TIMESTAMP	NOT NULL,
                           `updated_at`	TIMESTAMP	NOT NULL
 );
 
 CREATE TABLE `Schedule` (
-                            `schedule_id`	BIGINT AUTO_INCREMENT	NOT NULL,
+                            `schedule_id`	BIGINT AUTO_INCREMENT PRIMARY KEY,
                             `member_id`	BIGINT	NOT NULL,
                             `title`	TEXT	NOT NULL,
                             `contents`	TEXT	NULL,
@@ -19,40 +19,40 @@ CREATE TABLE `Schedule` (
                             `location_explanation`	VARCHAR(100)	NOT NULL,
                             `latitude`	DOUBLE	NOT NULL,
                             `longitude`	DOUBLE	NOT NULL,
-                            `status`	ENUM	NOT NULL,
+                            `status`	CHAR(15)	NOT NULL,
                             `remind_at`	TIMESTAMP	NOT NULL,
                             `created_at`	TIMESTAMP	NOT NULL,
                             `updated_at`	TIMESTAMP	NOT NULL
 );
 
 CREATE TABLE `Friend` (
-                          `id`	BIGINT AUTO_INCREMENT	NOT NULL,
+                          `id`	BIGINT AUTO_INCREMENT  PRIMARY KEY,
                           `member_id`	BIGINT	NOT NULL,
                           `member_req_id`	BIGINT	NOT NULL,
-                          `status`	ENUM	NOT NULL,
+                          `status`	CHAR(15)	NOT NULL,
                           `created_at`	TIMESTAMP	NOT NULL,
                           `updated_at`	TIMESTAMP	NOT NULL
 );
 
 CREATE TABLE `ScheduleFriend` (
-                                  `schedule_friend_id`	BIGINT AUTO_INCREMENT	NOT NULL,
+                                  `schedule_friend_id`	BIGINT AUTO_INCREMENT  PRIMARY KEY,
                                   `schedule_id`	BIGINT	NOT NULL,
                                   `member_id`	BIGINT	NOT NULL
 );
 
 CREATE TABLE `ChatRoom` (
-                            `chat_room_id`	BIGINT AUTO_INCREMENT	NOT NULL,
+                            `chat_room_id`	BIGINT AUTO_INCREMENT  PRIMARY KEY,
                             `schedule_id`	BIGINT	NOT NULL,
                             `member_id`	BIGINT	NOT NULL,
-                            `status`	ENUM	NOT NULL,
+                            `status`	CHAR(15)	NOT NULL,
                             `created_at`	TIMESTAMP	NOT NULL,
                             `updated_at`	TIMESTAMP	NOT NULL
 );
 
 CREATE TABLE `Notice` (
-                          `notice_id`	BIGINT AUTO_INCREMENT	NOT NULL,
+                          `notice_id`	BIGINT AUTO_INCREMENT  PRIMARY KEY,
                           `member_id`	BIGINT	NOT NULL,
-                          `type`	ENUM	NOT NULL,
+                          `type`	CHAR(15)	NOT NULL,
                           `information_id`	BIGINT	NULL,
                           `is_read`	BOOLEAN	NOT NULL,
                           `created_at`	TIMESTAMP	NOT NULL,
@@ -60,50 +60,14 @@ CREATE TABLE `Notice` (
 );
 
 CREATE TABLE `Chat` (
-                        `chat_room_message_id`	BIGINT AUTO_INCREMENT	NOT NULL,
+                        `chat_room_message_id`	BIGINT AUTO_INCREMENT  PRIMARY KEY,
                         `chat_room_id`	BIGINT	NOT NULL,
                         `member_id`	BIGINT	NOT NULL,
                         `created_at`	TIMESTAMP	NOT NULL,
                         `updated_at`	TIMESTAMP	NOT NULL,
-                        `content`	VARCHAR	NOT NULL
+                        `content`	VARCHAR(300)	NOT NULL
 );
 
-ALTER TABLE `Member` ADD CONSTRAINT `PK_MEMBER` PRIMARY KEY (
-                                                             `member_id`
-    );
-
-ALTER TABLE `Schedule` ADD CONSTRAINT `PK_SCHEDULE` PRIMARY KEY (
-                                                                 `schedule_id`,
-                                                                 `member_id`
-    );
-
-ALTER TABLE `Friend` ADD CONSTRAINT `PK_FRIEND` PRIMARY KEY (
-                                                             `id`,
-                                                             `member_id`
-    );
-
-ALTER TABLE `ScheduleFriend` ADD CONSTRAINT `PK_SCHEDULEFRIEND` PRIMARY KEY (
-                                                                             `schedule_friend_id`,
-                                                                             `schedule_id`,
-                                                                             `member_id`
-    );
-
-ALTER TABLE `ChatRoom` ADD CONSTRAINT `PK_CHATROOM` PRIMARY KEY (
-                                                                 `chat_room_id`,
-                                                                 `schedule_id`,
-                                                                 `member_id`
-    );
-
-ALTER TABLE `Notice` ADD CONSTRAINT `PK_NOTICE` PRIMARY KEY (
-                                                             `notice_id`,
-                                                             `member_id`
-    );
-
-ALTER TABLE `Chat` ADD CONSTRAINT `PK_CHAT` PRIMARY KEY (
-                                                         `chat_room_message_id`,
-                                                         `chat_room_id`,
-                                                         `member_id`
-    );
 
 ALTER TABLE `Schedule` ADD CONSTRAINT `FK_Member_TO_Schedule_1` FOREIGN KEY (
                                                                              `member_id`
@@ -167,4 +131,5 @@ ALTER TABLE `Chat` ADD CONSTRAINT `FK_Member_TO_Chat_1` FOREIGN KEY (
     REFERENCES `Member` (
                          `member_id`
         );
+
 
