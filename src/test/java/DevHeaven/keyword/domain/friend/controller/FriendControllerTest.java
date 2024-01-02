96417446e4d4f81ac16b3033505f0fbe2dcdd3f4
haebeen <haebing0309@gmail.com>
2024-01-02 22:42:04 +0900

    [Test #1] 친구 삭제 테스트 실패 코드 추가

--- src/test/java/DevHeaven/keyword/domain/friend/controller/FriendControllerTest.java
+++ src/test/java/DevHeaven/keyword/domain/friend/controller/FriendControllerTest.java
@@ -1,15 +1,19 @@
 package DevHeaven.keyword.domain.friend.controller;
 
+import static DevHeaven.keyword.common.exception.type.ErrorCode.*;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+import DevHeaven.keyword.common.exception.FriendNotFoundException;
+import DevHeaven.keyword.common.exception.MemberNotFoundException;
 import DevHeaven.keyword.domain.friend.service.FriendService;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -26,7 +30,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.payload.JsonFieldType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -43,15 +46,30 @@ class FriendControllerTest {
 
   @MockBean
   private FriendService friendService;
-
+  protected  ResourceSnippetParameters friendDeleteResponse() {
+    return ResourceSnippetParameters.builder()
+        .pathParameters(parameterWithName("memberId").description("친구 삭제할 Member Id"))
+        .responseFields(
+            fieldWithPath("errorCode").type(JsonFieldType.STRING).description("친구 삭제 실패 code"),
+            fieldWithPath("errorMessage").type(JsonFieldType.STRING).description("친구 삭제 실패 에러 메세지"),
+            fieldWithPath("httpStatus").type(JsonFieldType.STRING).description("친구 삭제 실패 http status")
+        )
+        .responseSchema(Schema.schema("Friend Delete Response"))
+        .build();
+  }
   @Test
   @DisplayName("친구 삭제 - 성공")
   void deleteFriend_success() throws Exception {
-    mockMvc.perform(delete("/friends/{memberId}",1L))
+    //given
+    Long memberId=1L;
+
+    //when
+    //then
+    mockMvc.perform(delete("/friends/{memberId}",memberId))
         .andDo(print())
         .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(jsonPath("$.friendDelete").value(true))
-        .andDo(MockMvcRestDocumentationWrapper.document("friends/delete",
+        .andExpect(jsonPath("$.isFriendDelete").value(true))
+        .andDo(MockMvcRestDocumentationWrapper.document("friends/delete/success",
             preprocessRequest(prettyPrint()),
             preprocessResponse(prettyPrint()),
             resource(
@@ -60,11 +78,58 @@ class FriendControllerTest {
                     .summary("친구 삭제 API")
                     .pathParameters(parameterWithName("memberId").description("친구 삭제할 Member Id"))
                     .responseFields(
-                        fieldWithPath("friendDelete").type(JsonFieldType.BOOLEAN).description("친구 삭제 성공 유무")
+                        fieldWithPath("isFriendDelete").type(JsonFieldType.BOOLEAN).description("친구 삭제 성공 유무")
                     )
                     .responseSchema(Schema.schema("Friend Delete Response"))
                     .build()
             )));
 
+    verify(friendService).deleteFriend(memberId);
+  }
+
+  @Test
+  @DisplayName("친구 삭제 - 실패 : 친구 목록에서 ACCEPT 인 상태를 찾을 수 없음")
+  void deleteFriend_fail_not_found_accepted() throws Exception {
+    //given
+    Long memberId=1L;
+
+    //when
+    doThrow(new FriendNotFoundException(FRIEND_NOT_FOUND)).when(friendService).deleteFriend(anyLong());
+
+    //then
+    mockMvc.perform(delete("/friends/{memberId}",memberId))
+        .andDo(print())
+        .andExpect(MockMvcResultMatchers.status().isNotFound())
+        .andDo(MockMvcRestDocumentationWrapper.document("friends/delete/fail/notFriend",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint()),
+            resource(
+                friendDeleteResponse()
+            )));
+
+    verify(friendService).deleteFriend(memberId);
+  }
+
+  @Test
+  @DisplayName("친구 삭제 - 실패 : 친구를 회원 목록에서 찾을 수 없음")
+  void deleteFriend_fail_not_found_member() throws Exception {
+    //given
+    Long memberId = 1L;
+
+    //when
+    doThrow(new MemberNotFoundException(MEMBER_NOT_FOUND)).when(friendService).deleteFriend(anyLong());
+
+    //then
+    mockMvc.perform(delete("/friends/{memberId}",memberId))
+        .andDo(print())
+        .andExpect(MockMvcResultMatchers.status().isNotFound())
+        .andDo(MockMvcRestDocumentationWrapper.document("friends/delete/fail/notMember",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint()),
+            resource(
+                friendDeleteResponse()
+            )));
+
+    verify(friendService).deleteFriend(memberId);
   }
 }
