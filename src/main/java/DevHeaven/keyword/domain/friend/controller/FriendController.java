2db3e708aa981c0dce53a7d34d11bf52a3a04f85
haebeen <haebing0309@gmail.com>
2024-01-05 07:40:54 +0900

    [Refactor #10] FriendController 및 테스트 코드 부분 수정

--- src/main/java/DevHeaven/keyword/domain/friend/controller/FriendController.java
+++ src/main/java/DevHeaven/keyword/domain/friend/controller/FriendController.java
@@ -19,9 +19,6 @@ public class FriendController {
   // TODO : 시큐리티 적용후 @AuthPrincipal 파라미터 추가 예정
   @DeleteMapping("/{memberReqId}")
   public ResponseEntity <FriendDeleteResponse> deleteFriend(@PathVariable(required = true,name = "memberReqId") final Long memberRequestId){
-    friendService.deleteFriend(memberRequestId);
-    return ResponseEntity.ok(FriendDeleteResponse.builder()
-        .isFriendDelete(true)
-        .build());
+    return ResponseEntity.ok(friendService.deleteFriend(memberRequestId));
   }
 }
