731e077a2c5da974d595d9e7ac9dd83c29170e47
Jennny1 <june9282@gmail.com>
2024-01-12 01:31:34 +0900

    [Feat #3] schedulefriend 테이블명 추가

--- src/main/java/DevHeaven/keyword/domain/schedule/entity/ScheduleFriend.java
+++ src/main/java/DevHeaven/keyword/domain/schedule/entity/ScheduleFriend.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,6 +20,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "schedulefriend")
 public class ScheduleFriend {
 
     @Id
