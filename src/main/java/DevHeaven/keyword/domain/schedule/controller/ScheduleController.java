8ef7c62de744fb34aa486573ef69ae8369a78c2f
Jennny1 <june9282@gmail.com>
2024-01-16 01:27:51 +0900

    [Feat #41] 스케줄 생성 기능 구현

--- src/main/java/DevHeaven/keyword/domain/schedule/controller/ScheduleController.java
+++ src/main/java/DevHeaven/keyword/domain/schedule/controller/ScheduleController.java
@@ -1,6 +1,8 @@
 package DevHeaven.keyword.domain.schedule.controller;
 
 import DevHeaven.keyword.domain.member.dto.MemberAdapter;
+import DevHeaven.keyword.domain.schedule.dto.request.ScheduleCreateRequest;
+import DevHeaven.keyword.domain.schedule.dto.response.ScheduleCreateResponse;
 import DevHeaven.keyword.domain.schedule.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
@@ -8,6 +10,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +28,13 @@ public class ScheduleController {
         return ResponseEntity.ok(
                 scheduleService.getScheduleList(memberAdapter, pageable));
     }
+
+    @PostMapping
+    public ResponseEntity<ScheduleCreateResponse> createSchedule(
+        @RequestBody ScheduleCreateRequest request,
+        @AuthenticationPrincipal MemberAdapter memberAdapter) {
+
+        return ResponseEntity.ok(scheduleService.createSchedule(request, memberAdapter));
+    }
+
 }
--- src/main/java/DevHeaven/keyword/domain/schedule/service/ScheduleService.java
+++ src/main/java/DevHeaven/keyword/domain/schedule/service/ScheduleService.java
@@ -1,12 +1,17 @@
 package DevHeaven.keyword.domain.schedule.service;
 
 import DevHeaven.keyword.common.exception.MemberException;
+import DevHeaven.keyword.common.exception.type.ErrorCode;
 import DevHeaven.keyword.domain.member.dto.MemberAdapter;
 import DevHeaven.keyword.domain.member.entity.Member;
 import DevHeaven.keyword.domain.member.repository.MemberRepository;
+import DevHeaven.keyword.domain.member.service.MemberService;
+import DevHeaven.keyword.domain.schedule.dto.request.ScheduleCreateRequest;
+import DevHeaven.keyword.domain.schedule.dto.response.ScheduleCreateResponse;
 import DevHeaven.keyword.domain.schedule.dto.response.ScheduleListResponse;
 import DevHeaven.keyword.domain.schedule.entity.Schedule;
 import DevHeaven.keyword.domain.schedule.repository.ScheduleRepository;
+import DevHeaven.keyword.domain.schedule.type.ScheduleStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -23,6 +28,7 @@ import static DevHeaven.keyword.common.exception.type.ErrorCode.EMAIL_NOT_FOUND;
 public class ScheduleService {
     private final MemberRepository memberRepository;
     private final ScheduleRepository scheduleRepository;
+    private final MemberService memberService;
 
     public Page<ScheduleListResponse> getScheduleList(
             MemberAdapter memberAdapter,
@@ -40,5 +46,39 @@ public class ScheduleService {
     private Member getMemberByEmail(final String email) {
         return memberRepository.findByEmail(email)
                 .orElseThrow(() -> new MemberException(EMAIL_NOT_FOUND));
+    }
+
+    public ScheduleCreateResponse createSchedule(ScheduleCreateRequest request,
+        MemberAdapter memberAdapter) {
+
+        Member member = memberRepository.findByEmail(memberAdapter.getEmail())
+            .orElseThrow(() -> new MemberException(EMAIL_NOT_FOUND));
+
+        // friend 상태 확인
+
+        for (Member friend : request.getScheduleFriendList()) {
+            Member friendInfomation = memberRepository.findById(friend.getMemberId())
+                .orElseThrow(() -> new MemberException(ErrorCode.MEMBER_NOT_FOUND));
+
+            memberService.validateMemberByStatus(friendInfomation);
+
+        }
+
+        Schedule schedule = Schedule.builder()
+            .member(member)
+            .title(request.getTitle())
+            .contents(request.getContents())
+            .scheduleAt(request.getScheduleAt())
+            .locationExplanation(request.getLocationExplanation())
+            .latitude(request.getLatitude())
+            .longitude(request.getLongitude())
+            .status(ScheduleStatus.ONGOING)
+            .remindAt(request.getRemindAt())
+            .friendList(request.getScheduleFriendList())
+            .build();
+
+        scheduleRepository.save(schedule);
+
+        return ScheduleCreateResponse.builder().scheduleId(schedule.getScheduleId()).build();
     }
 }
