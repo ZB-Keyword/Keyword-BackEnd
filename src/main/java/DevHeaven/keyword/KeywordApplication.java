8f8cd3217fc64e4cf2144e793fdb687399115082
haebeen <haebing0309@gmail.com>
2024-01-21 23:48:01 +0900

    [Refactor #87] aws 관련한 exclude 제거

--- src/main/java/DevHeaven/keyword/KeywordApplication.java
+++ src/main/java/DevHeaven/keyword/KeywordApplication.java
@@ -5,13 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication(
-		exclude = {
-				org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
-				org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
-				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
-		}
-)
+@SpringBootApplication
 public class KeywordApplication {
 
 	public static void main(String[] args) {
