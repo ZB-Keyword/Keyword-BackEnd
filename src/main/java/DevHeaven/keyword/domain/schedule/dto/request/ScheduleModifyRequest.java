4b0a92671c882e07a87f00438e1ce72ce44c6b71
haebeen <haebing0309@gmail.com>
2024-01-22 23:14:59 +0900

    [Refactor #98] 스케줄 부분 수정 가능하게 변경

--- src/main/java/DevHeaven/keyword/domain/schedule/dto/request/ScheduleModifyRequest.java
+++ src/main/java/DevHeaven/keyword/domain/schedule/dto/request/ScheduleModifyRequest.java
@@ -2,25 +2,37 @@ package DevHeaven.keyword.domain.schedule.dto.request;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ScheduleModifyRequest {
 
+  @NotNull
   private String title;
 
+  @NotNull
   private String contents;
 
+  @NotNull
   private LocalDateTime scheduleAt;
 
+  @NotNull
   private String locationExplanation;
 
+  @NotNull
   private Double latitude;
 
+  @NotNull
   private Double longitude;
 
+  @NotNull
   private Long remindAt;
 
   private List<ScheduleFriendRequest> scheduleFriendList;
