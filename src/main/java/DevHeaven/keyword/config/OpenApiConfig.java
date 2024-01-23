cf2d700766477fda4bb692f89f63469c75f8b4c8
haebeen <haebing0309@gmail.com>
2024-01-23 14:27:04 +0900

    [Refactor #103] 시큐리티 헤더 테스트중

--- src/main/java/DevHeaven/keyword/config/OpenApiConfig.java
+++ src/main/java/DevHeaven/keyword/config/OpenApiConfig.java
@@ -1,15 +1,19 @@
 package DevHeaven.keyword.config;
 
-
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 
-@SecurityScheme(type = SecuritySchemeType.APIKEY, name = "Authorization",
-    in = SecuritySchemeIn.HEADER)
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "My API", version = "v1"))
+@SecurityScheme(
+    name = "basicAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "basic"
+)
 public class OpenApiConfig {
 
 
