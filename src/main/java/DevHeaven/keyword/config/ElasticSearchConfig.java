fed4e9040a3832c21c1b51f9168b97fcaf0f2e62
haebeen <haebing0309@gmail.com>
2024-01-29 22:20:02 +0900

    [Feature #69] elasticsearchConfig 수정

--- src/main/java/DevHeaven/keyword/config/ElasticSearchConfig.java
+++ src/main/java/DevHeaven/keyword/config/ElasticSearchConfig.java
@@ -2,6 +2,7 @@ package DevHeaven.keyword.config;
 
 import DevHeaven.keyword.domain.friend.repository.ElasticSearchRepository;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
@@ -12,10 +13,12 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories(basePackageClasses = ElasticSearchRepository.class)
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
+    @Value("${spring.elasticsearch.url}")
+    private String elasticUrl;
     @Override
     public RestHighLevelClient elasticsearchClient() {
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+                .connectedTo(elasticUrl)
                 .build();
         return RestClients.create(clientConfiguration).rest();
     }
