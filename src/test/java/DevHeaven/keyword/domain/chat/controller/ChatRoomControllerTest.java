abb140a31dba33e38dc9015b2153b67eb63ae669
soeun135 <soensuckun@naver.com>
2024-02-08 03:04:10 +0900

    [Test #15] 채팅방 목록 조회 테스트

--- src/test/java/DevHeaven/keyword/domain/chat/controller/ChatRoomControllerTest.java
+++ src/test/java/DevHeaven/keyword/domain/chat/controller/ChatRoomControllerTest.java
@@ -1,7 +1,10 @@
 package DevHeaven.keyword.domain.chat.controller;
 
+import DevHeaven.keyword.common.exception.ChatException;
+import DevHeaven.keyword.common.exception.type.ErrorCode;
 import DevHeaven.keyword.common.exception.util.ResponseUtils;
 import DevHeaven.keyword.common.security.JwtUtils;
+import DevHeaven.keyword.domain.chat.dto.response.ChatRoomListResponse;
 import DevHeaven.keyword.domain.chat.service.ChatRoomService;
 import DevHeaven.keyword.domain.chat.service.ChatService;
 import DevHeaven.keyword.domain.member.dto.MemberAdapter;
@@ -19,9 +22,17 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static DevHeaven.keyword.domain.member.type.MemberRole.MEMBER;
 import static DevHeaven.keyword.domain.member.type.MemberStatus.ACTIVE;
@@ -29,11 +40,17 @@ import static com.epages.restdocs.apispec.ResourceDocumentation.headerWithName;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ChatRoomController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -57,32 +74,19 @@ class ChatRoomControllerTest {
     @MockBean
     private MemberRepository memberRepository;
 
-    private ResourceSnippetParameters friendDeleteErrorResponse() {
-        return ResourceSnippetParameters.builder()
-                .tag("Chat API")
-                .summary("채팅방 생성 API")
-                .requestHeaders(
-                        headerWithName("Authorization").description("Bearer AccessToken")
-                )
-                .pathParameters(parameterWithName("memberId").description("친구 삭제할 Member Id"))
-                .responseFields(
-                        fieldWithPath("errorCode").type(JsonFieldType.STRING).description("친구 삭제 실패 code"),
-                        fieldWithPath("errorMessage").type(JsonFieldType.STRING).description("친구 삭제 실패 에러 메세지"),
-                        fieldWithPath("httpStatus").type(JsonFieldType.STRING).description("친구 삭제 실패 http status")
-                )
-                .responseSchema(Schema.schema("Friend Delete Response"))
-                .build();
-    }
     @Test
     @DisplayName("채팅방 생성 - 성공")
     @WithCustomMockUser
-    void createChatRoom() throws Exception{
+    void createChatRoom() throws Exception {
         //given
-        MemberAdapter memberAdapter = MemberAdapter.from(MemberFixture.DOG.createMember());
+        MemberAdapter memberAdapter =
+                MemberAdapter.from(
+                        MemberFixture.DOG.createMember());
 
         Long scheduleId = 1L;
 
-        given(chatRoomService.createChatRoom(memberAdapter, scheduleId))
+        given(chatRoomService.createChatRoom(
+                memberAdapter, scheduleId))
                 .willReturn(true);
 
         //when
@@ -93,7 +97,7 @@ class ChatRoomControllerTest {
 
                 )
                 .andDo(print())
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string("true"))
                 .andDo(MockMvcRestDocumentationWrapper.document("chats/room/create/success",
                         ResourceSnippetParameters.builder()
@@ -124,9 +128,10 @@ class ChatRoomControllerTest {
         //then
         mockMvc.perform(
                         post("/chats/room/{scheduleId}", scheduleId)
+                                .header("Authorization", "Bearer AccessToken")
                 )
                 .andDo(print())
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string("false"))
                 .andDo(MockMvcRestDocumentationWrapper.document("chats/room/create/fail",
                         ResourceSnippetParameters.builder()
@@ -142,7 +147,88 @@ class ChatRoomControllerTest {
     }
 
     @Test
-    void getChatRoomList() {
+    @DisplayName("채팅방 목록 가져오기")
+    @WithCustomMockUser
+    void getChatRoomList() throws Exception {
+        //given
+        MemberAdapter memberAdapter =
+                MemberAdapter.from(MemberFixture.DOG.createMember());
+
+        Pageable pageable = PageRequest.of(0, 10);
+
+        List<String> friendsName = Arrays.asList("버니", "벅스", "강아지");
+
+        ChatRoomListResponse response1 =
+                ChatRoomListResponse.builder()
+                        .chatRoomId(1L)
+                        .scheduleTitle("크리스마스 모임")
+                        .friendsName(friendsName)
+                        .build();
+        ChatRoomListResponse response2 =
+                ChatRoomListResponse.builder()
+                        .chatRoomId(2L)
+                        .scheduleTitle("생일파티")
+                        .friendsName(friendsName)
+                        .build();
+        ChatRoomListResponse response3 =
+                ChatRoomListResponse.builder()
+                        .chatRoomId(3L)
+                        .scheduleTitle("송별회")
+                        .friendsName(friendsName)
+                        .build();
+
+        Page<ChatRoomListResponse> pageResult =
+                new PageImpl<>(Arrays.asList(response1, response2, response3));
+
+        given(chatRoomService.getChatRoomList(memberAdapter, pageable))
+                .willReturn(pageResult);
+        //when
+        //then
+        mockMvc.perform(
+                        get("/chats/room")
+                                .header("Authorization", "Bearer AccessToken")
+                                .param("size", "10")
+                                .param("page", "0")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content.[0].scheduleTitle").value("크리스마스 모임"))
+                .andDo(MockMvcRestDocumentationWrapper.document("chats/room",
+                        ResourceSnippetParameters.builder()
+                                .tag("Chat API")
+                                .summary("채팅방 목록 조회 API")
+                                .description("로그인한 사용자가 포함된 채팅방 목록 조회")
+                                .responseSchema(Schema.schema("ChatRoomListResponse"))
+                        , preprocessRequest(prettyPrint())
+                        , preprocessResponse(prettyPrint())
+                        , requestParameters(
+                                parameterWithName("size").description("Description of size parameter"),
+                                parameterWithName("page").description("Description of page parameter")
+                        )
+                        , responseFields(
+                                fieldWithPath("content[].chatRoomId").description("채팅방 번호")
+                                , fieldWithPath("content[].scheduleTitle").description("일정 제목")
+                                , fieldWithPath("content[].friendsName").description("일정 참여 멤버")
+
+                                , fieldWithPath("pageable").type(JsonFieldType.STRING).description("페이지 정보")
+                                , fieldWithPath("last").description("마지막 페이지인지 여부")
+                                , fieldWithPath("totalElements").description("테이블 총 데이터 갯수")
+                                , fieldWithPath("totalPages").description("전체 페이지 갯수")
+                                , fieldWithPath("size").description("한 페이지당 조회할 데이터 갯수")
+                                , fieldWithPath("number").description("현재 페이지 번호")
+
+                                , fieldWithPath("sort.empty").description("데이터 비었는지 여부")
+                                , fieldWithPath("sort.sorted").description("정렬 여부")
+                                , fieldWithPath("sort.unsorted").description("정렬 안 됐는지 여부")
+
+                                , fieldWithPath("first").description("첫 번째 페이지인지 여부")
+                                , fieldWithPath("numberOfElements").description("요청 페이지에서 조회된 데이터 갯수")
+                                , fieldWithPath("empty").description("데이터 비었는지 여부")
+
+                        )
+
+
+                ));
     }
 
     @Test
