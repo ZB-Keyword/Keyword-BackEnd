c404edeb064a3afab5fb4095cd71b0412faa5756
ParkHaeBeen <130157565+ParkHaeBeen@users.noreply.github.com>
2024-02-14 18:23:56 +0900

    Update KeywordApplication.java

--- src/main/java/DevHeaven/keyword/KeywordApplication.java
+++ src/main/java/DevHeaven/keyword/KeywordApplication.java
@@ -5,14 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication(
-		exclude = {
-				org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
-				org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
-				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
-		}
-)
-//@PropertySource("classpath:config/application-prod.yml")
+@SpringBootApplication
 public class KeywordApplication {
 
 	public static void main(String[] args) {
