f000acd83055908b91dba8dfaa082cf16534b5d8
HJune <59690831+Jennny1@users.noreply.github.com>
2024-02-18 07:25:39 +0900

    fix(프론트 연결 과정에서 수정사항 반영)

--- src/main/java/DevHeaven/keyword/common/exception/util/ResponseUtils.java
+++ src/main/java/DevHeaven/keyword/common/exception/util/ResponseUtils.java
@@ -2,21 +2,26 @@ package DevHeaven.keyword.common.exception.util;
 
 import DevHeaven.keyword.common.exception.dto.ErrorResponse;
 import DevHeaven.keyword.common.exception.type.ErrorCode;
-import DevHeaven.keyword.domain.member.dto.OAuthMemberAdapter;
-import DevHeaven.keyword.domain.member.service.MemberService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+@Lazy
 public class ResponseUtils {
 
   private final ObjectMapper objectMapper;
+
+  @Autowired
+  public ResponseUtils(ObjectMapper objectMapper) {
+    this.objectMapper = objectMapper;
+  }
+
 
   public void sendErrorResponse(final HttpServletResponse response, final ErrorCode errorCode)
       throws IOException {
@@ -29,4 +34,5 @@ public class ResponseUtils {
   private String getJsonByErrorCode(final ErrorCode errorCode) throws JsonProcessingException {
     return objectMapper.writeValueAsString(ErrorResponse.from(errorCode));
   }
+
 }
--- src/main/java/DevHeaven/keyword/config/WebMvcConfig.java
+++ src/main/java/DevHeaven/keyword/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package DevHeaven.keyword.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -15,14 +20,29 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
+
+  @Autowired
+  @Lazy
+  @Qualifier("jacksonObjectMapper")
+  private ObjectMapper jacksonObjectMapper;
+
   @Bean
   public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
     MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-    converter.setObjectMapper(objectMapper());
+    converter.setObjectMapper(jacksonObjectMapper);
     return converter;
   }
 
+  @Bean(name = "jacksonObjectMapper")
+  @Lazy
+  public ObjectMapper jacksonObjectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+    return objectMapper;
+  }
+
   @Bean
+  @Primary
   public ObjectMapper objectMapper() {
     return new ObjectMapper();
   }
